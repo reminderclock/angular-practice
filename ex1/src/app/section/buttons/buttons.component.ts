3ef475eac950ad45cd533ede629dad9e574d1c14
Luke <reminderclock@naver.com>
2021-12-06 08:46:55 +0900

    feat: ..ing

--- ex1/src/app/section/buttons/buttons.component.ts
+++ ex1/src/app/section/buttons/buttons.component.ts
@@ -7,11 +7,10 @@ import { Component, OnInit, Output,EventEmitter } from '@angular/core';
 })
 export class ButtonsComponent implements OnInit {
   // 이컴포넌트는 clickEvent라고 이름 지어진 이벤트를 만들수 있다. 
-  test=0;
   @Output() clickEvent = new EventEmitter<string>();
   constructor() { }
   start(){
-   this.clickEvent.emit('change');
+   this.clickEvent.emit('start');
   }
 
   ngOnInit(): void {
--- ex1/src/app/section/time-display/time-display.component.ts
+++ ex1/src/app/section/time-display/time-display.component.ts
@@ -8,18 +8,30 @@ import { Component, Input, OnInit, SimpleChange } from '@angular/core';
 export class TimeDisplayComponent implements OnInit {
   // 배열을 초기화 해주어야 한다.
   @Input() inputData: string= "";
-  test = 1;
+  // timeText:string ="00:00.00";
+  minutes:number = 0;
+  seconds:number = 0;
+  miliSeconds: number = 0;
+  timeInterval:any
   constructor() { 
     // console.log(this.inputData)
     // setInterval(()=> {
     //   this.test++;
     // },1000)
   }
+  timeStart(){
+    this.timeInterval = setInterval(() => {
 
+    },10)
+    clearInterval()
+  }
   ngOnChanges(changes: SimpleChange){
     console.log(changes)
     for(let propName in changes){
-
+      if(propName === "inputData" && changes[propName].currentValue == "start") {
+        this.timeStart()
+        }
+      }
     }
   }
   ngOnInit(): void {
